10c1056beb6cf352e18e869d5d4f7e586d701d1a
afnmo <128582924+afnmo@users.noreply.github.com>
2024-05-19 12:03:23 +0300

    Update registerForm.js

--- javaScript/registerForm.js
+++ javaScript/registerForm.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
 import { getFirestore, getDoc, query, where, doc, updateDoc, addDoc,getDocs, collection, deleteDoc } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js';
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyAWSHSS6v0pG5VxrmfXElArcMpjBT5o6hg",
@@ -10,20 +9,17 @@ const firebaseConfig = {
     messagingSenderId: "18569998394",
     appId: "1:18569998394:web:c8efa4c8b656702c1cc503"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 // Access Firestore
 const db = getFirestore(app);
-
 // Specify the name of the collection you want to read from
 const collectionName = "Station_Requests";
-
 //retrieve stationID
 const SID = sessionStorage.getItem('branchManagerID');
 
 const sessionID = sessionStorage.getItem('sessionID');
+// const sessionID = sessionStorage.getItem('sessionID');
 
 function showToast(message) {
     // Create a toast element
@@ -32,14 +28,11 @@ function showToast(message) {
     toastElement.role = 'alert';
     toastElement.setAttribute('aria-live', 'assertive');
     toastElement.setAttribute('aria-atomic', 'true');
-
     toastElement.style.backgroundColor = '#30b476'; 
-
     // Create the toast body
     const toastBody = document.createElement('div');
     toastBody.className = 'toast-body';
     toastBody.textContent = message;
-
     // Create a close button
     const closeButton = document.createElement('button');
     closeButton.type = 'button';
@@ -49,18 +42,14 @@ function showToast(message) {
         // Hide the toast when close button is clicked
         toastElement.style.display = 'none';
     });
-
     // Append the close button and body to the toast
     toastElement.appendChild(toastBody);
     toastElement.appendChild(closeButton);
-
     // Append the toast to the toast container
     const toastContainer = document.getElementById('toast-container');
     toastContainer.appendChild(toastElement);
-
     // Show the toast
     toastElement.style.display = 'block';
-
     // Automatically hide the toast after a certain duration (e.g., 5 seconds)
     // Automatically hide the toast after a certain duration (e.g., 5 seconds)
 setTimeout(function() {
@@ -69,40 +58,34 @@ setTimeout(function() {
         toastContainer.removeChild(toastElement); // Remove the toast from the DOM
     }, 500); // Adjust the duration as needed to match the CSS transition
 }, 5000); // Adjust the duration as needed
-
 }
-
-
 async function chechRejected(){
     console.log("chechRejected() entered");
 try {
     // Retrieve the branchManagerID from sessionStorage
     // const SID = sessionStorage.getItem('branchManagerID');
-
     // Create a reference to the Station_Requests collection
     const stationRequestsRef = collection(db, 'Station_Requests');
 
     console.log("sessionID" + sessionID);
+    console.log("sessionID" + SID);
     // Query for documents where branch_manager_id matches SID
     const querySnapshot = await getDocs(query(stationRequestsRef, where('branch_manager_id', '==', sessionID)));
+    const querySnapshot = await getDocs(query(stationRequestsRef, where('branch_manager_id', '==', SID)));
     // console.log(querySnapshot);
 
     querySnapshot.forEach(async (doc) => {
         // Extract the value of the 'accepted' field from each document
         const acceptedValue = doc.data().accepted;
-
         // Check the value of the 'accepted' field
         if (acceptedValue === 'declined') {
             // Get the requestId
             const requestId = doc.id;
-
             // Call the updateRequestStatus function with appropriate message
             // Provide feedback to the user
             const message = "Your station registration request has been declined by admin. You can register again or contact admin for further details: admin@91.com."
-
             // Show a toast notification to the user
             showToast(message);
-
             // await updateRequestStatus(requestId, false, "");
             await deleteDoc(doc.ref);
         }
@@ -114,8 +97,10 @@ try {
 }
 
 if (sessionID) {
+if (SID) {
     chechRejected();
     const Sdoc = doc(db, "Branch_Manager", sessionID); // Update the document reference
+    const Sdoc = doc(db, "Branch_Manager", SID); // Update the document reference
     // Use await with getDoc since it returns a Promise
     const docSnap = await getDoc(Sdoc);
 
@@ -127,20 +112,16 @@ if (sessionID) {
         document.getElementById("FirstName").value = SData.firstName;
         document.getElementById("LastName").value = SData.lastName;
         document.getElementById("Email").value = SData.email;
-
         document.getElementById("FirstName").style.fontSize = "larger";
         document.getElementById("LastName").style.fontSize = "larger";
         document.getElementById("Email").style.fontSize = "larger";
         document.getElementById("FirstName").style.color = "#30b476";
         document.getElementById("LastName").style.color = "#30b476";
         document.getElementById("Email").style.color = "#30b476";
-
     }
 } else {
     window.location.href = "signup.html";
 }
-
-
 // if(sessionID){
 //     console.log("sessionID true");
 // chechRejected();
@@ -150,7 +131,6 @@ if (sessionID) {
 // }
 const urlInput = document.getElementById("GoogleMapUrl");
 const urlError = document.getElementById("urlError");
-
 async function Addrequests() {
     const stationName = document.getElementById("StationName").value;
     const stationLocation = document.getElementById("GoogleMapUrl").value;
@@ -163,7 +143,6 @@ async function Addrequests() {
   
     
         const isValidURL = validateURL(stationLocation);
-
         if (!isValidURL || stationLocation.trim() === '') {
             urlError.textContent = "Location must be in Google Maps URL format";
             urlInput.classList.add('error');
@@ -172,28 +151,26 @@ async function Addrequests() {
  
     
     try {
-
-
         // Add to the "Station_Requests" collection
         const stationRequestRef = await addDoc(collection(db, "Station_Requests"), {
             name: stationName,
             location: stationLocation,
             branch_manager_id: sessionID, 
+            branch_manager_id: SID, 
             accepted: "pending", //'pending',
             requestDate: requestDate
         });        
-
         // Get the ID of the newly created "Branch_Manager" document
         const stationRequestID = stationRequestRef.id;
 
         // Define a reference to a specific document within the "Branch_Manager" collection
         const branchManagerDocRef = doc(db, "Branch_Manager", sessionID);
+        const branchManagerDocRef = doc(db, "Branch_Manager", SID);
 
         // Update the document with the new field
         // updateDoc(branchManagerDocRef, {
         //     station_request_id: stationRequestID,
         // });
-
         // Document updated successfully
         document.getElementById("registrationForm").reset();
         // window.location.href = "login.html";
@@ -203,18 +180,13 @@ async function Addrequests() {
     }
       
 }
-
 document.getElementById("registrationForm").addEventListener("submit", async function (event) {
     event.preventDefault();
-
         await Addrequests();
 });
-
 function validateURL(url) {
     const urlPattern = /^https:\/\/maps\.app\.goo\.gl\/.*$/;
-
     const isValidURL = urlPattern.test(url);
-
     if (!isValidURL) {
         urlError.textContent = "Location must be in the format 'https://maps.app.goo.gl/'";
         urlInput.classList.add('error');
@@ -222,6 +194,5 @@ function validateURL(url) {
         urlError.textContent = "";
         urlInput.classList.remove('error');
     }
-
     return isValidURL;
 }
